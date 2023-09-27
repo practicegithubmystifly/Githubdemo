d5672f52dee7e03561c9914b51f9915f4c873b4f
trupti.d <trupti.d@MYSTILAP-322.mystifly.net>
2023-09-27 13:23:33 +0530

    First commit for even odd number

---
+++ src/Gitdemo1/Even_Odd_Num.java
@@ -0,0 +1,16 @@
+package Gitdemo1;
+
+public class Even_Odd_Num 
+{
+	public static void main(String[] args) {
+        int num=19;
+        if (num % 2 == 0) {
+            System.out.println(" is even.");
+        } else {
+            System.out.println(" is odd.");
+        }
+        
+  
+    }
+}
+
